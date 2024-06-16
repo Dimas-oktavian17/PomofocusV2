d8af5337a92ec9dcffbecee0dbbe741abffb7ae0
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-06-17 00:22:14 +0700

    feat: create status component

--- components/base/StatusLabel.vue
+++ components/base/StatusLabel.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const data = ref(true)
+</script>
 
 <template>
-  <div>
-    Component: base/StatusLabel
-  </div>
+  <UButton @click="data = !data" size="sm"
+    :class="[data ? 'text-red-900 bg-red-200 border border-red-900 border-solid rounded-full fill-red-800 hover:bg-red-100' : 'text-green-900 bg-green-200 border border-green-900 border-solid rounded-full fill-green-800 hover:bg-green-100']"
+    class="duration-1000 hover:transition-all">
+    <UIcon name="i-ph-brain" dynamic v-if="data" />
+    <UIcon name="i-ph-coffee" dynamic v-else />
+    {{ data ? 'Focus' : 'break' }}
+  </UButton>
 </template>
-
-<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-    stater
+    <BaseStatusLabel />
 </template>
