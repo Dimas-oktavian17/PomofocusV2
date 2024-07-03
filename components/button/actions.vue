abf34ca65340254cc30201d4b9e31382e8ad7f99
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-07-03 11:43:02 +0700

    feat: creating key function, ts , eslint config

--- components/button/actions.vue
+++ components/button/actions.vue
@@ -1,18 +1,18 @@
 <script setup>
-const emit = defineEmits(['update', 'check', 'tes'])
+const emit = defineEmits(['settings', 'check', 'tes'])
 </script>
 <template>
   <section class="grid grid-cols-3 place-items-center">
-    <UButton @click="$emit('update')"
-      class="p-6 duration-1000 bg-red-200 text-red-950 rounded-3xl hover:bg-red-100 hover:transition-all">
+    <UButton class="p-6 duration-1000 bg-red-200 text-red-950 rounded-3xl hover:bg-red-100 hover:transition-all"
+      @click="$emit('settings')">
       <UIcon name="i-ph-dots-three-outline-fill" dynamic class="size-5" />
     </UButton>
-    <UButton @click="$emit('check')"
-      class="py-6 duration-1000 bg-red-400 px-7 text-red-950 rounded-3xl hover:bg-red-300 hover:transition-all">
+    <UButton class="py-6 duration-1000 bg-red-400 px-7 text-red-950 rounded-3xl hover:bg-red-300 hover:transition-all"
+      @click="$emit('check')">
       <UIcon name="i-ph-pause-fill" dynamic class="size-7" />
     </UButton>
-    <UButton @click="$emit('tes')"
-      class="p-6 duration-1000 bg-red-200 text-red-950 rounded-3xl hover:bg-red-100 hover:transition-all">
+    <UButton class="p-6 duration-1000 bg-red-200 text-red-950 rounded-3xl hover:bg-red-100 hover:transition-all"
+      @click="$emit('tes')">
       <UIcon name="i-ph-fast-forward-fill" dynamic class="size-5" />
     </UButton>
   </section>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,54 @@
 <script setup>
-const data = ref()
-const re = data.value * 60
-const res = computed(() => Math.floor(data.value / 60))
+import { useModalStore } from '~/stores/modal/useModal';
+// const { counter } = useInterval(1000, { controls: true })
+// const useCounter = useCounterStore()
+const useModal = useModalStore()
+const { isOpen } = storeToRefs(useModal)
+const { metaSymbol } = useShortcuts()
+// watchEffect(() => useCounter.counter === 4 ? useCounter.counter = 1 : 0)
+const useMenus = useMenu()
+onKeyStroke('k', (event) => {
+    if (event.ctrlKey) {
+        event.preventDefault()
+        isOpen.value = true
+    }
+});
 </script>
 <template>
-    <input type="number" v-model="data">
-    {{ data }}
     <UCard class="grid max-w-sm grid-cols-1 border-none shadow-none place-items-center ring-0 bg-red-50">
         <template #header>
             <BaseStatusLabel />
         </template>
         <h1 class="font-normal text-red-950 text-9xl">
-            {{ res }}
+            <!-- {{ convertSecondsToMinutes(counter) }} -->
         </h1>
         <template #footer>
-            <ButtonActions @update="console.log('update')" @tes="console.log('tes')" @check="console.log('cek')" />
+            <ButtonActions @settings="isOpen = true" />
         </template>
+        <UModal v-model="isOpen">
+            <UCard class=" bg-red-50">
+                <section class="flex flex-col ">
+                    <div v-for="({ title, kbd, icon }) in useMenus" :key="icon"
+                        class="grid grid-cols-2 place-items-center">
+                        <UButton
+                            class="text-red-900 bg-transparent shadow-none outline-none ring-0 place-self-start hover:bg-transparent"
+                            size="sm">
+                            <UIcon :name="icon" dynamic class="size-7" />
+                            {{ title }}
+                        </UButton>
+                        <div
+                            class="py-[10px] text-red-300 bg-transparent shadow-none place-self-end hover:bg-transparent">
+                            <UKbd class="text-red-300 bg-transparent border border-red-300 outline-none ring-0">
+                                {{ metaSymbol }}
+                            </UKbd>
+                            +
+                            <UKbd class="text-red-300 bg-transparent border border-red-300 outline-none ring-0">
+                                {{ kbd }}
+                            </UKbd>
+                        </div>
+                    </div>
+                </section>
+            </UCard>
+        </UModal>
     </UCard>
 </template>
